26fd24018e13e1d0c012d91e0386dd0c23177773
Eric Zhang <ericspring08@gmail.com>
2023-12-13 17:20:54 -0600

    add missing files

---
+++ src/utils.py
@@ -0,0 +1,40 @@
+# Models
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier, RandomForestClassifier, ExtraTreesClassifier, BaggingClassifier, HistGradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB, CategoricalNB, ComplementNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from catboost import CatBoostClassifier
+from sklearn.linear_model import RidgeClassifier, PassiveAggressiveClassifier, SGDOneClassSVM
+from sklearn.dummy import DummyClassifier
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+
+
+model_options = {
+    'SVC': SVC(),
+    'GradientBoosting': GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,max_depth=5, random_state=0),
+    'GaussianNB': GaussianNB(),
+    'DecisionTree': DecisionTreeClassifier(),
+    'KNeighbors': KNeighborsClassifier(n_neighbors=5),
+    'AdaBoost': AdaBoostClassifier(),
+    'RandomForest': RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1, random_state=42),
+    'MLP': MLPClassifier(alpha=2, max_iter=1000, random_state=42),
+    'QDA': QuadraticDiscriminantAnalysis(),
+    'CatBoost': CatBoostClassifier(iterations=2,
+                                   learning_rate=1,
+                                   depth=2, verbose=False),
+    'ExtraTrees': ExtraTreesClassifier(),
+    'Bagging': BaggingClassifier(),
+    'Ridge': RidgeClassifier(),
+    'PA': PassiveAggressiveClassifier(),
+    'SGDOneClass': SGDOneClassSVM(),
+    'Dummy': DummyClassifier(),
+    'HGB': HistGradientBoostingClassifier(),
+    'LGBM': LGBMClassifier(),
+    'XGB': XGBClassifier()
+}
+
+metrics = ['accuracy', 'precision', 'balanced_accuracy']
