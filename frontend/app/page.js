4cb93bcab406bc811d802685e49912028b45f8cd
Surrealzz <91282211+Surrealzz@users.noreply.github.com>
2023-12-20 00:59:27 +0000

    another test

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By Benjamin Standefer and Eric Zhang{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
