35f05d2b54454cd09d909695bd76a4ddd8056cf9
Surrealzz <91282211+Surrealzz@users.noreply.github.com>
2023-12-22 06:04:40 +0000

    small fix

--- frontend/app/cardio/BloodWorkForm1.js
+++ frontend/app/cardio/BloodWorkForm1.js
@@ -73,7 +73,7 @@ const HeartDiseaseForm = () => {
           type="submit"
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
         >
-          Submit
+          Next
         </button>
       </div>
       </Link>
