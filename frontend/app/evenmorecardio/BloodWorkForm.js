4afaa99c7f323d995b4579986a013126a8d05c68
Surrealzz <91282211+Surrealzz@users.noreply.github.com>
2023-12-23 07:11:17 +0000

    changes

--- frontend/app/evenmorecardio/BloodWorkForm.js
+++ frontend/app/evenmorecardio/BloodWorkForm.js
@@ -59,8 +59,8 @@ const HeartDiseaseForm = () => {
     }
   };
 
-  return (
-    <form onSubmit={handleSubmit} className="max-w-lg mx-auto mt-8">
+  return (  
+      <form onSubmit={handleSubmit} className="max-w-lg mx-auto mt-8">
       <div className="mb-4">
         <label htmlFor="restingECG" className="block text-gray-700 font-semibold mb-2">
           Resting ECG
@@ -75,13 +75,84 @@ const HeartDiseaseForm = () => {
           className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
         />
       </div>
-      {/* Rest of the form inputs remain the same */}
-      
+      <div className="mb-4">
+        <label htmlFor="maxHeartRate" className="block text-gray-700 font-semibold mb-2">
+          Maximum Heart Rate
+        </label>
+        <input
+          type="number"
+          id="maxHeartRate"
+          name="maxHeartRate"
+          value={formData.maxHeartRate}
+          onChange={handleChange}
+          placeholder="Enter maximum heart rate"
+          className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="oldpeak" className="block text-gray-700 font-semibold mb-2">
+          Oldpeak
+        </label>
+        <input
+          type="number"
+          id="oldpeak"
+          name="oldpeak"
+          value={formData.oldpeak}
+          onChange={handleChange}
+          placeholder="Enter oldpeak"
+          className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="slope" className="block text-gray-700 font-semibold mb-2">
+          Slope of Peak Exercise ST Segment
+        </label>
+        <input
+          type="number"
+          id="slope"
+          name="slope"
+          value={formData.slope}
+          onChange={handleChange}
+          placeholder="Enter slope"
+          className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="numMajorVessels" className="block text-gray-700 font-semibold mb-2">
+          Number of Major Vessels Colored by Fluorosopy
+        </label>
+        <input
+          type="number"
+          id="numMajorVessels"
+          name="numMajorVessels"
+          value={formData.numMajorVessels}
+          onChange={handleChange}
+          placeholder="Enter number of major vessels"
+          className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="thal" className="block text-gray-700 font-semibold mb-2">
+          Thal
+        </label>
+        <select
+          id="thal"
+          name="thal"
+          value={formData.thal}
+          onChange={handleChange}
+          className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
+        >
+          <option value="">Select Thal</option>
+          <option value="normal">Normal</option>
+          <option value="fixed defect">Fixed Defect</option>
+          <option value="reversible defect">Reversible Defect</option>
+        </select>
+      </div>
       <Link href={respLink}>
         <div className="mt-6">
           <button
-            type="submit"
             onClick={sendToServer}
+            type="submit"
             className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
           >
             Next
@@ -91,5 +162,6 @@ const HeartDiseaseForm = () => {
     </form>
   );
 };
+
 
 export default HeartDiseaseForm;
