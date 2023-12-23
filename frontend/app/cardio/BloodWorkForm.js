750c0820c3372204f548feda6ff3c35439008bd1
Surrealzz <91282211+Surrealzz@users.noreply.github.com>
2023-12-23 04:35:36 +0000

    small fix

--- frontend/app/cardio/BloodWorkForm.js
+++ frontend/app/cardio/BloodWorkForm.js
@@ -4,10 +4,10 @@ import Link from 'next/link';
 const HeartDiseaseForm = () => {
   const respLink = 'https://nasahunch.vercel.app/morecardio';
   const [formData, setFormData] = useState({
-    age: '',
     sex: '',
+    age: '',
     chestPain: '',
-    restingBloodPressure: '',
+    exerciseInducedAngina: '',
   });
 
   const handleChange = (e) => {
@@ -27,20 +27,6 @@ const HeartDiseaseForm = () => {
   return (
     <form onSubmit={handleSubmit} className="max-w-lg mx-auto mt-8">
       <div className="mb-4">
-        <label htmlFor="age" className="block text-gray-700 font-semibold mb-2">
-          Age
-        </label>
-        <input
-          type="number"
-          id="age"
-          name="age"
-          value={formData.age}
-          onChange={handleChange}
-          placeholder="Enter age"
-          className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
-        />
-      </div>
-      <div className="mb-4">
         <label htmlFor="sex" className="block text-gray-700 font-semibold mb-2">
           Sex
         </label>
@@ -57,6 +43,20 @@ const HeartDiseaseForm = () => {
         </select>
       </div>
       <div className="mb-4">
+        <label htmlFor="age" className="block text-gray-700 font-semibold mb-2">
+          Age
+        </label>
+        <input
+          type="number"
+          id="age"
+          name="age"
+          value={formData.age}
+          onChange={handleChange}
+          placeholder="Enter age"
+          className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-4">
         <label htmlFor="chestPain" className="block text-gray-700 font-semibold mb-2">
           Chest Pain
         </label>
@@ -66,23 +66,25 @@ const HeartDiseaseForm = () => {
           name="chestPain"
           value={formData.chestPain}
           onChange={handleChange}
-          placeholder="Enter chest pain level (0-3)"
+          placeholder="Enter chest pain level (numerical)"
           className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
         />
       </div>
       <div className="mb-4">
-        <label htmlFor="restingBloodPressure" className="block text-gray-700 font-semibold mb-2">
-          Resting Blood Pressure
+        <label htmlFor="exerciseInducedAngina" className="block text-gray-700 font-semibold mb-2">
+          Exercise Induced Angina
         </label>
-        <input
-          type="number"
-          id="restingBloodPressure"
-          name="restingBloodPressure"
-          value={formData.restingBloodPressure}
+        <select
+          id="exerciseInducedAngina"
+          name="exerciseInducedAngina"
+          value={formData.exerciseInducedAngina}
           onChange={handleChange}
-          placeholder="Enter resting blood pressure"
           className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
-        />
+        >
+          <option value="">Do you have exercise-induced angina?</option>
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
       </div>
       <Link href={respLink}>
         <div className="mt-6">
