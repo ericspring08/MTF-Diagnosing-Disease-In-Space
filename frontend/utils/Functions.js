844cf4fa66f9e5b9a23c85c575ec4675b9fc54ac
Surrealzz <91282211+Surrealzz@users.noreply.github.com>
2023-12-26 01:36:13 +0000

    blah

--- frontend/utils/Functions.js
+++ frontend/utils/Functions.js
@@ -9,17 +9,16 @@ export const createPDF = (
 ) => {
   const doc = new jsPDF();
 
-  // Set font styles
+
   doc.setFont('helvetica', 'normal');
 
-  // Title
   doc.setFontSize(24);
   doc.setTextColor(33, 150, 243); // Blue color
   doc.text(`Diagnosis Report: ${disease}`, 20, 30);
 
-  // Subtitle
+
   doc.setFontSize(16);
-  doc.setTextColor(0); // Reset to black
+  doc.setTextColor(0); 
   doc.text(`Diagnosis: ${diagnosis}`, 20, 45);
   doc.text(
     `Diagnosis Confidence: ${(diagnosis_confidence * 100).toFixed(2)}%`,
@@ -27,18 +26,18 @@ export const createPDF = (
     60
   );
 
-  // Features Section
+ 
   doc.setFontSize(14);
   let posY = 80;
   for (const [key, value] of Object.entries(features)) {
-    doc.setTextColor(33, 33, 33); // Darker color for feature names
+    doc.setTextColor(33, 33, 33); 
     doc.text(`${key}:`, 20, posY);
-    doc.setTextColor(77, 77, 77); // Lighter color for feature values
+    doc.setTextColor(77, 77, 77); 
     doc.text(`${value}`, 40, posY);
     posY += 15;
   }
 
-  // Footer with Date
+
   const currentDate = new Date();
   const date = `${currentDate.getFullYear()}-${
     currentDate.getMonth() + 1
@@ -47,6 +46,6 @@ export const createPDF = (
   doc.setFontSize(10);
   doc.text(`Generated on: ${date}`, 20, 280);
 
-  // Save the PDF with a formatted file name
+  
   doc.save(`${disease}_Diagnosis_Report_${date}.pdf`);
 };
