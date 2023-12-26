3a6a1aecd4a718cd1ff55f84487bdf1f644aab2d
Surrealzz <91282211+Surrealzz@users.noreply.github.com>
2023-12-26 02:42:43 +0000

    pdf fixing further

--- frontend/utils/Functions.js
+++ frontend/utils/Functions.js
@@ -1,5 +1,4 @@
-
-import { jsPDF } from 'jspdf';
+/*import { jsPDF } from 'jspdf';
 
 export const createPDF = (
   disease,
@@ -49,3 +48,50 @@ export const createPDF = (
   
   doc.save(`${disease}_Diagnosis_Report_${date}.pdf`);
 };
+*/
+import { jsPDF } from 'jspdf';
+
+export const createPDF = (
+  disease,
+  features,
+  diagnosis,
+  diagnosis_confidence,
+) => {
+  const doc = new jsPDF();
+
+  doc.setFont('helvetica', 'normal');
+
+  doc.setFontSize(24);
+  doc.setTextColor(33, 150, 243); // Blue color
+  doc.text(`Diagnosis Report: ${disease}`, 20, 30);
+
+  doc.setFontSize(16);
+  doc.setTextColor(0);
+  doc.text(`Diagnosis: ${diagnosis === '0' ? 'Positive' : 'Negative'}`, 20, 45);
+  doc.text(
+    `Diagnosis Confidence: ${(diagnosis_confidence * 100).toFixed(2)}%`,
+    20,
+    60
+  );
+
+  doc.setFontSize(14);
+  let posY = 80;
+  for (const [key, value] of Object.entries(features)) {
+    doc.setTextColor(33, 33, 33);
+    doc.text(`${key}:`, 20, posY);
+    doc.setTextColor(77, 77, 77);
+    doc.text(`${value}`, 40, posY);
+    posY += 15;
+  }
+
+  const currentDate = new Date();
+  const date = `${currentDate.getFullYear()}-${
+    currentDate.getMonth() + 1
+  }-${currentDate.getDate()}`;
+  doc.setTextColor(100);
+  doc.setFontSize(10);
+  doc.text(`Generated on: ${date}`, 20, 280);
+
+  doc.save(`${disease}_Diagnosis_Report_${date}.pdf`);
+};
+
