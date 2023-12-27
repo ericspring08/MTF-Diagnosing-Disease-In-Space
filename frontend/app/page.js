b677a92711790aa52cdfecde76cad544e90134ff
Eric Zhang <ericspring08@gmail.com>
2023-12-26 23:17:21 -0600

    add navbar and remove old code

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,46 +1,47 @@
-
 'use client';
 
 import React, { useEffect } from 'react';
 import Link from 'next/link';
 import axios from 'axios';
+import Navbar from '../utils/Navbar';
 
 const HomePage = () => {
-  const [diseases, setDiseases] = React.useState(null);
+     const [diseases, setDiseases] = React.useState(null);
 
-  useEffect(() => {
-    let promise = axios.get('https://nasahunchapi.onrender.com/diseases');
-    promise.then((response) => {
-      setDiseases(response.data.diseases);
-    });
-  }, []);
+     useEffect(() => {
+          let promise = axios.get('https://nasahunchapi.onrender.com/diseases');
+          promise.then((response) => {
+               setDiseases(response.data.diseases);
+          });
+     }, []);
 
-  const DiseaseButtons = () => {
-    if (diseases === null) return null;
-    else {
-      return (
-        <div className="flex flex-wrap justify-center items-center">
-          {diseases.map((disease) => (
-            <Link legacyBehavior href={`/form/${disease}`} key={disease}>
-              <button className="btn btn-warning px-6 py-2">
-                {disease}
-              </button>
-            </Link>
-          ))}
-        </div>
-      );
-      
-    }
-  };
+     const DiseaseButtons = () => {
+          if (diseases === null) return null;
+          else {
+               return (
+                    <div className="flex flex-wrap justify-center items-center">
+                         {diseases.map((disease) => (
+                              <Link
+                                   legacyBehavior
+                                   href={`/form/${disease}`}
+                                   key={disease}
+                              >
+                                   <button className="btn btn-warning px-6 py-2">
+                                        {disease}
+                                   </button>
+                              </Link>
+                         ))}
+                    </div>
+               );
+          }
+     };
 
-  return (
-    <div className="h-screen flex flex-col justify-center items-center" data-theme="corporate">
-      <div className="lg:text-8xl md:text-5xl sm:text-5xl text-5xl p-10 text-center">
-        Smart Disease Diagnosis
-      </div>
-      <DiseaseButtons />
-    </div>
-  );
+     return (
+          <div data-theme="corporate">
+               <Navbar />
+               <DiseaseButtons />
+          </div>
+     );
 };
 
 export default HomePage;
---
+++ frontend/utils/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+     return (
+          <div className="navbar bg-primary">
+               <a className="btn btn-ghost text-xl text-white">
+                    Smart Diagnosis
+               </a>
+          </div>
+     );
+};
+
+export default Navbar;
