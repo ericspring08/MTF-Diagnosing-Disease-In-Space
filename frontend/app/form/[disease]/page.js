fae9cf9f30bad65328929f583006ba9099b9b251
Eric Zhang <ericspring08@gmail.com>
2024-01-04 08:11:12 -0600

    add images

--- frontend/app/form/[disease]/page.js
+++ frontend/app/form/[disease]/page.js
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
 import { createPDF } from '../../../utils/Functions';
-import { i18n } from '@/next.config';
+import Form from './form';
 
 const Page = ({ params }) => {
   const [formIndex, setFormIndex] = React.useState(0);
@@ -262,104 +262,6 @@ const Page = ({ params }) => {
   );
 };
 
-const Form = ({
-  formStructure,
-  formHeaders,
-  formIndex,
-  formData,
-  setFormData,
-}) => {
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(() => ({
-      ...formData,
-      [name]: value,
-    }));
-  };
 
-  if (formHeaders.length === 0) {
-    <span className="loading loading-dots loading-xs"></span>;
-  } else {
-    return (
-      <div className="max-w-lg mx-auto m-5" key={formIndex}>
-        <h1 className="text-2xl font-semibold mb-4">
-          {formHeaders[formIndex]}
-        </h1>
-        {Object.entries(formStructure[formHeaders[formIndex]]).map(
-          ([key, value]) => {
-            return (
-              <div key={key} className="my-3">
-                {value.type === 'numerical' ? (
-                  <div>
-                    <label
-                      htmlFor={key}
-                      className="label-text"
-                    >
-                      {value.title}
-                    </label>
-                    <input
-                      type="number"
-                      id={key}
-                      name={key}
-                      value={formData[key]}
-                      onChange={handleChange}
-                      placeholder={`Enter ${value.title}`}
-                      className="input input-bordered w-full"
-                    />
-                  </div>
-                ) : null}
-                {value.type === 'categorical' ? (
-                  <div>
-                    <label
-                      htmlFor={key}
-                      className="label-text"
-                    >
-                      {value.title}
-                    </label>
-                    <select
-                      id={key}
-                      name={key}
-                      value={formData[key]}
-                      onChange={handleChange}
-                      className="select select-bordered w-full"
-                    >
-                      <option value="">
-                        Please select{' '}
-                        {value.title}
-                      </option>
-                      {Object.entries(
-                        value.options,
-                      ).map(
-                        ([
-                          optionname,
-                          optionvalue,
-                        ]) => {
-                          return (
-                            <option
-                              value={
-                                optionvalue
-                              }
-                              key={
-                                optionname
-                              }
-                            >
-                              {
-                                optionname
-                              }
-                            </option>
-                          );
-                        },
-                      )}
-                    </select>
-                  </div>
-                ) : null}
-              </div>
-            );
-          },
-        )}
-      </div>
-    );
-  }
-};
 
 export default Page;
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -6,72 +6,72 @@ import Image from 'next/image';
 import axios from 'axios';
 
 const HomePage = () => {
-     const [diseases, setDiseases] = React.useState(null);
+  const [diseases, setDiseases] = React.useState(null);
 
-     useEffect(() => {
-          let promise = axios.get('/api/diseases');
-          promise.then((response) => {
-               setDiseases(response.data.diseases);
-          });
-     }, []);
+  useEffect(() => {
+    let promise = axios.get('/api/diseases');
+    promise.then((response) => {
+      setDiseases(response.data.diseases);
+    });
+  }, []);
 
-     const DiseaseCards = () => {
-          if (diseases === null)
-               return <span className="loading loading-dots loading-lg"></span>;
-          else {
-               return (
-                    <div className="flex flex-wrap justify-center items-stretch">
-                         {diseases.map((disease) => (
-                              <Link
-                                   className="flex flex-wrap justify-center items-stretch m-3"
-                                   href={`/form/${disease.value}`}
-                                   key={disease.value}
-                              >
-                                   <div className="card card-bordered w-80 bg-base-100 shadow-xl hover:shadow-2xl hover:opacity-60">
-                                        <figure className="px-10 pt-10">
-                                             <Image
-                                                  src={`/img/${disease.value}.png`}
-                                                  alt={disease}
-                                                  width={500}
-                                                  height={500}
-                                             />
-                                        </figure>
-                                        <div className="card-body items-center text-center">
-                                             <h2 className="card-title">
-                                                  {disease.label}
-                                             </h2>
-                                             <p className="text-sm">
-                                                  {disease.description}
-                                             </p>
-                                        </div>
-                                   </div>
-                              </Link>
-                         ))}
-                    </div>
-               );
-          }
-     };
+  const DiseaseCards = () => {
+    if (diseases === null)
+      return <span className="loading loading-dots loading-lg"></span>;
+    else {
+      return (
+        <div className="flex flex-wrap justify-center items-stretch">
+          {diseases.map((disease) => (
+            <Link
+              className="flex flex-wrap justify-center items-stretch m-3"
+              href={`/form/${disease.value}`}
+              key={disease.value}
+            >
+              <div className="card card-bordered w-80 bg-base-100 shadow-xl hover:shadow-2xl hover:opacity-60">
+                <figure className="px-10 pt-10">
+                  <Image
+                    src={`/img/${disease.value}.png`}
+                    alt={disease}
+                    width={500}
+                    height={500}
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">
+                    {disease.label}
+                  </h2>
+                  <p className="text-sm">
+                    {disease.description}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      );
+    }
+  };
 
-     return (
-          <div
-               className="h-screen w-screen flex flex-col items-center"
-               data-theme="corporate"
-          >
-               <h1
-                    className="text-5xl text-center font-bold mt-10 mb-5"
-                    data-theme="corporate"
-               >
-                    Welcome to the Disease Diagnosis System
-               </h1>
-               <h2
-                    className="text-3xl text-center mt-5 mb-10"
-                    data-theme="corporate"
-               >
-                    Select a disease to diagnose
-               </h2>
-               <DiseaseCards />
-          </div>
-     );
+  return (
+    <div
+      className="h-screen w-screen flex flex-col items-center"
+      data-theme="corporate"
+    >
+      <h1
+        className="text-5xl text-center font-bold mt-10 mb-5"
+        data-theme="corporate"
+      >
+        Welcome to the Disease Diagnosis System
+      </h1>
+      <h2
+        className="text-3xl text-center mt-5 mb-10"
+        data-theme="corporate"
+      >
+        Select a disease to diagnose
+      </h2>
+      <DiseaseCards />
+    </div>
+  );
 };
 
 export default HomePage;
