b8e5ade5e3248cd3506584d7b5936746f0f7c6b5
Eric Zhang <ericspring08@gmail.com>
2024-03-15 22:05:39 -0500

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,14 +6,14 @@ const nextConfig = () => {
         source: '/api/:path*',
         destination: 'https://nasahunchapi.onrender.com/:path*',
         // destination: 'http://127.0.0.1:8000/:path*',
-        experimental: {
-          serverComponentsExternalPackages: ['@react-pdf/renderer'],
-        },
       },
     ];
   };
   return {
     rewrites,
+    experimental: {
+      serverComponentsExternalPackages: ['@react-pdf/renderer'],
+    }
   };
 };
 
