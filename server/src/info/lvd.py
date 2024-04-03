24c023d317bee5087fcf4f25dc06420006e48dd1
Eric Zhang <ericspring08@gmail.com>
2024-04-03 09:28:13 -0500

    begin server implementation of lvd

---
+++ server/src/info/lvd.py
@@ -0,0 +1,36 @@
+lvd_features = ["Age",
+                "Total_Bilirubin",
+                "Direct_Bilirubin",
+                "Alkaline_Phosphotase",
+                "Alamine_Aminotransferase",
+                "Aspartate_Aminotransferase",
+                "Total_Protiens",
+                "Albumin",
+                "Albumin_and_Globulin_Ratio",
+                "Gender"]
+lvd_numerical_features = ["Age",
+                          "Total_Bilirubin",
+                          "Direct_Bilirubin",
+                          "Alkaline_Phosphotase",
+                          "Alamine_Aminotransferase",
+                          "Aspartate_Aminotransferase",
+                          "Total_Protiens",
+                          "Albumin",
+                          "Albumin_and_Globulin_Ratio"]
+lvd_categorical_features = ["Gender"]
+lvd_types = {
+    "Age": int,
+    "Total_Bilirubin": float,
+    "Direct_Bilirubin": float,
+    "Alkaline_Phosphotase": int,
+    "Alamine_Aminotransferase": int,
+    "Aspartate_Aminotransferase": int,
+    "Total_Protiens": float,
+    "Albumin": float,
+    "Albumin_and_Globulin_Ratio": float,
+    "Gender": str
+}
+
+lvd_form = {
+
+}
