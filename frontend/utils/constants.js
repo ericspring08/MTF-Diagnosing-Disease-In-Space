ff3785656bcf9a2fe2e797c879feca3e7e8a9b64
Eric Zhang <ericspring08@gmail.com>
2024-04-06 15:52:24 -0500

    update constants

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,4 +1,4 @@
-export const DISEASES = ['hdd', 'kdd', 'ldd', 'tdd'];
+export const DISEASES = ['hdd', 'kdd', 'ldd', 'tdd', 'lvd'];
 export const DISEASE_COLOR_MAP = {
   'disease': 'blue-600',
   'sensor': 'green-500'
@@ -7,5 +7,6 @@ export const DISEASE_NAMES = {
   'hdd': 'Heart Disease',
   'kdd': 'Kidney Disease',
   'ldd': 'Lung Disease',
-  'tdd': 'Thyroid Disease'
+  'tdd': 'Thyroid Disease',
+  'lvd': 'Liver Disease'
 }
